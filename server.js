af447b64d967d3445bd1a1f1a3fb016c86282045
Matt <b5010228@my.shu.ac.uk>
2019-03-17 18:12:34 +0000

    added second token handler

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, x-access-token, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, x-access-token, x-access-token2, Content-Type, Accept");
     next();
   });
 app.use(bodyParser.urlencoded({ extended: true }));
