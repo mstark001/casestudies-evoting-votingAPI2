5639d19cbea03304072f0293df8701e0c945edcd
Matt <b5010228@my.shu.ac.uk>
2019-03-17 18:26:14 +0000

    fixed bug

--- app/routes/GetUserFromToken.js
+++ app/routes/GetUserFromToken.js
@@ -3,10 +3,10 @@ var config = require('../../config/enviromentVariables');
 
 
 async function getUserFromToken(req, res) {
-  var token = req.headers['x-access-token'];
+  var token = req.headers['x-access-token2'];
   if (!token)
     return null;
-  return await jwt.verify(token, config.secret, function(err, decoded) {
+  return await jwt.verify(token, config.localSecret, function(err, decoded) {
     if (err)
       return null;
 
